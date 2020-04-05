70dd6a5c8bef0069e4babe38b5e11cbceb81f924
Elena Dinca <elenadinca25@gmail.com>
2020-04-05 13:21:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -38,8 +38,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  result[1][:name] = "Joelle van Dyne (PGOAT)"
-  result
+  aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  aoh[1][:name]
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
